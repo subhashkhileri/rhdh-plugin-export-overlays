e1c50f05a1300ce287005a75aff0d724155a3253
David Festal <dfestal@redhat.com>
2025-05-27 22:39:40 +0200

    Bootstrap the content of the `main` branch based on workspaces merged on branch `release-1.5`
    Signed-off-by: David Festal <dfestal@redhat.com>

---
+++ workspaces/tech-radar/plugins/tech-radar/overlay/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  techRadarPlugin,
+  techRadarPlugin as plugin,
+  TechRadarPage,
+} from './plugin';
+
+export * from './components';
+
+export * from './api';
+
+export { default as TechRadarIcon } from '@material-ui/icons/MyLocation';
