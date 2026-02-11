381a05eded859893d99ae439ffac0ba4507a1ef5
Subhash Khileri <skhileri@redhat.com>
2026-02-11 23:37:51 +0530

    Update tech-radar e2e-tests dependencies and improve nodejs version detection

--- workspaces/tech-radar/e2e-tests/tests/specs/deploy-customization-provider.sh
+++ workspaces/tech-radar/e2e-tests/tests/specs/deploy-customization-provider.sh
@@ -7,9 +7,13 @@ deploy_test_backstage_customization_provider() {
 
   # Check if the buildconfig already exists
   if ! oc get buildconfig test-backstage-customization-provider -n "${project}" > /dev/null 2>&1; then
-    echo "Creating new app for test-backstage-customization-provider"
-    oc new-app -S openshift/nodejs:18-minimal-ubi8
-    oc new-app https://github.com/janus-qe/test-backstage-customization-provider --image-stream="openshift/nodejs:18-ubi8" --namespace="${project}"
+    # Get latest nodejs UBI9 tag from cluster, fallback to 18-ubi8
+    local nodejs_tag
+    nodejs_tag=$(oc get imagestream nodejs -n openshift -o jsonpath='{.spec.tags[*].name}' 2> /dev/null \
+      | tr ' ' '\n' | grep -E '^[0-9]+-ubi9$' | sort -t'-' -k1 -n | tail -1)
+    nodejs_tag="${nodejs_tag:-18-ubi8}"
+    echo "Creating new app for test-backstage-customization-provider using nodejs:${nodejs_tag}"
+    oc new-app "openshift/nodejs:${nodejs_tag}~https://github.com/janus-qe/test-backstage-customization-provider" --namespace="${project}"
   else
     echo "BuildConfig for test-backstage-customization-provider already exists in ${project}. Skipping new-app creation."
   fi
