5b829700809cca6494a438f625338feae2671eb1
Jan Richter <jrichter@redhat.com>
2026-03-06 14:08:37 +0100

    chore(github-issues): migrate plugin tests to overlays (#2000)
    Signed-off-by: Jan Richter <jrichter@redhat.com>

---
+++ workspaces/github-issues/e2e-tests/playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "@red-hat-developer-hub/e2e-test-utils/playwright-config";
+import dotenv from "dotenv";
+
+dotenv.config({ path: `${import.meta.dirname}/.env` });
+process.env.SKIP_KEYCLOAK_DEPLOYMENT = "true";
+
+export default defineConfig({
+  projects: [
+    {
+      name: "github-issues",
+    },
+  ],
+});
