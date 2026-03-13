cf4d028a36ef274e27682c4e4b231325e24e7e5b
Patrick Knight <pknight@redhat.com>
2026-03-13 08:49:28 -0400

    chore(rbac): migrate RBAC plugin tests from RHDH (#1987)
    * chore(rbac): migrate RBAC plugin tests from RHDH
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * chore(rbac): code clean up and test fixes
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * chore(rbac): fix sonarqube issues
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * chore(rbac): fix eol and remove version from RHDH deployement
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * chore(rbac): handle review suggestions
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * chore(rbac): bump test utils and remove auth and rbac apis
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    * chore(rbac): remove dynamic-plugins.yaml file
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>
    
    ---------
    
    Signed-off-by: Patrick Knight <pknight@redhat.com>

---
+++ workspaces/rbac/e2e-tests/playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "@red-hat-developer-hub/e2e-test-utils/playwright-config";
+import dotenv from "dotenv";
+
+dotenv.config({ path: `${import.meta.dirname}/.env` });
+
+export default defineConfig({
+  projects: [
+    {
+      name: "rbac",
+    },
+  ],
+});
