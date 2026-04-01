84355a2866757169e2134aa25cd67ec66b194954
Sanket Pathak <sanketpathak95@gmail.com>
2026-04-01 15:13:48 +0530

    chore: migration of tekton plugin tests (#1982)
    Migration of tekton plugin tests

---
+++ workspaces/tekton/e2e-tests/tests/specs/tekton.spec.ts
@@ -0,0 +1,53 @@
+import { test } from "@red-hat-developer-hub/e2e-test-utils/test";
+import { $ } from "@red-hat-developer-hub/e2e-test-utils/utils";
+import * as path from "node:path";
+import { TektonSupportHelper } from "../support/tekton-support-helper";
+
+const operatorInstallPath = path.resolve(
+  process.cwd(),
+  "tests/config/operator-install.sh",
+);
+
+test.describe("Test Tekton plugin", () => {
+  test.beforeAll(async ({ rhdh }) => {
+    await rhdh.configure({
+      auth: "keycloak",
+    });
+    const namespace = rhdh.deploymentConfig.namespace;
+    // operator-install.sh: Tekton/Pipelines operator + waits, then namespace Active wait, pipeline-tests + RBAC (see operator::grant_default_service_account_cluster_reader_and_tekton).
+    await $`bash ${operatorInstallPath} ${namespace}`;
+    await rhdh.deploy();
+  });
+
+  test.beforeEach(async ({ loginHelper }) => {
+    await loginHelper.loginAsKeycloakUser();
+  });
+
+  test("Check Pipeline Run", async ({ page, uiHelper }) => {
+    const tekton = new TektonSupportHelper(page);
+    await tekton.goToBackstageJanusProjectCITab();
+    await tekton.ensurePipelineRunsTableIsNotEmpty();
+    await uiHelper.verifyHeading("Pipeline Runs");
+    await uiHelper.verifyTableHeadingAndRows(
+      tekton.getAllGridColumnsTextForPipelineRunsTable(),
+    );
+  });
+
+  test("Check search functionality", async ({ page }) => {
+    const tekton = new TektonSupportHelper(page);
+    await tekton.goToBackstageJanusProjectCITab();
+    await tekton.search("hello-world");
+    await tekton.ensurePipelineRunsTableIsNotEmpty();
+  });
+
+  test("Check if modal is opened after click on the pipeline stage", async ({
+    page,
+  }) => {
+    const tekton = new TektonSupportHelper(page);
+    await tekton.goToBackstageJanusProjectCITab();
+    await tekton.clickOnExpandRowFromPipelineRunsTable();
+    await tekton.openModalEchoHelloWorld();
+    await tekton.verifyModalOpened();
+    await tekton.checkPipelineStages(["echo-hello-world", "echo-bye"]);
+  });
+});
