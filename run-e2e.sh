b3d07887445f83c2129e336d35bf6b4a4a1abc75
Subhash Khileri <skhileri@redhat.com>
2026-04-28 14:37:22 +0530

    fix: pin RHDH_VERSION to 1.10-114-CI due to RHDHBUGS-3030 (#2326)

--- run-e2e.sh
+++ run-e2e.sh
@@ -32,7 +32,8 @@ cd "$SCRIPT_DIR"
 # These use defaults that can be overridden via environment variables.
 
 # RHDH deployment
-export RHDH_VERSION="${RHDH_VERSION:-1.10}"             # RHDH version to deploy (e.g., "1.10", "next")
+# export RHDH_VERSION="${RHDH_VERSION:-1.10}"             # RHDH version to deploy (e.g., "1.10", "next")
+export RHDH_VERSION="1.10-114-CI"                       # Pinned due to RHDHBUGS-3030
 export INSTALLATION_METHOD="${INSTALLATION_METHOD:-helm}" # "helm" or "operator"
 
 # Playwright
